bad75141fcfc3aa24fbe0663f4399eaa6f6512be
Adel <adelnmo2003@gmail.com>
2024-11-23 21:35:21 +0330

    Map data structure

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@
 
 // data structure: Array, Object, Set, Map
 
+// Set
 const array = [1, 2, 3, 3, 2, 4, 4, 5];
 
 // const uniq = new Set(array);
 const uniq = [...new Set(array)];
 
-console.log(uniq);
+// console.log(uniq);
 
 /*
 // Set methods
@@ -21,3 +22,45 @@ uniq.delete(3);
 
 console.log(uniq);
 */
+
+// Map
+// const obj = {
+//   true: "number is valid",
+//   25: "hello",
+// };
+// console.log(Object.keys(obj));
+
+// const ageMessageValidation = new Map([
+//   [true, "your age is valid"],
+//   [false, "your age is not valid"],
+// ]);
+
+// const myMap = new Map([
+//   ["car", "Nissan"],
+//   ["name", "Saeed"],
+// ]);
+
+// myMap.set(25, "something");
+// myMap.set("some", "something else");
+
+// console.log(myMap.get(25));
+// console.log(myMap.size);
+
+// myMap.delete("car");
+
+// console.log(myMap.has(25));
+// console.log(myMap.has("2"));
+
+// console.log(myMap);
+
+const numbers = new Map([
+  [1, "one"],
+  [2, "tow"],
+  [3, "three"],
+  [4, "four"],
+  [5, "five"],
+]);
+
+for (let i = 1; i <= 5; i++) {
+  console.log(numbers.ge4t(i));
+}

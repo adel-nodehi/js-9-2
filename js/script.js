2be42d098711467507fd261201a3fd8a4069d3d1
Adel <adelnmo2003@gmail.com>
2024-11-23 20:38:46 +0330

    Set data structure

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+// data structure: Array, Object, Set, Map
+
+const array = [1, 2, 3, 3, 2, 4, 4, 5];
+
+// const uniq = new Set(array);
+const uniq = [...new Set(array)];
+
+console.log(uniq);
+
+/*
+// Set methods
+uniq.add(25);
+
+console.log(uniq.size);
+console.log(uniq.has(6));
+
+// uniq.clear();
+uniq.delete(3);
+
+console.log(uniq);
+*/
